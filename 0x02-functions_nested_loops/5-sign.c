96cc0d940fe8a10ba7567e940a70946765f67b2e
Mziwakx <ayoubmzi10@gmail.com>
2023-06-20 09:19:56 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,10 @@
 #include "main.h"
-
 /**
  * print_sign - prints the sign of a number
  * @n: the number to be a checked
  * Return: 1 for positive num, -1 for nagative num or zero for anything else
  */
-
-int print_sign(int n)
-	
+int print_sign(int n)	
 {
 	if (n > 0)
 	{
