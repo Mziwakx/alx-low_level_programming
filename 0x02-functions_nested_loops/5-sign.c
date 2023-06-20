7c0da2e297ecf0ccb35eddcdcea30423a8005996
Mziwakx <ayoubmzi10@gmail.com>
2023-06-20 16:07:15 -0700

    task5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,22 @@
 #include "main.h"
+
 /**
- * print_sign - prints the sign of a number
- * @n: the number to be a checked
- * Return: 1 for positive num, -1 for nagative num or zero for anything else
+ * more_numbers - prints numbers between 0 to 14
+ * 10 times.
+ * Return: no return.
  */
-int print_sign(int n)	
+void more_numbers(void)
 {
-	if (n > 0)
+	int i, ch;
+
+	for (i = 0; i < 10; i++)
 	{
-	 _putchar(43);
-	 return (1);
-        }
-        else if (n < 0)
-	{
-	_putchar(45);
-	return (-1);
-	}
-	else
-	{
-	_putchar(48);
-	return (0);
+		for (ch = 0; ch < 15; ch++)
+		{
+			if (ch >= 10)
+				_putchar((ch / 10) + 48);
+			_putchar((ch % 10) + 48);
+		}
+		_putchar('\n');
 	}
 }
