d5d08d21451ba22c44cd11ca6ec258dd2079e91b
Mziwakx <ayoubmzi10@gmail.com>
2023-06-20 16:56:59 -0700

    taskolaslo5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
-* print_sign - prints the sign of a number
-* @n: the number to be a checked
-* Return: 1 for positive num, -1 for nagative num or zero for anything else
+*print_sign - prints the sign of a number
+*@n: the number to be a checked
+*Return: 1 for positive num, -1 for nagative num or zero for anything else
 */
 int print_sign(int n)
 {
