bbba2749b1105450d552ee49380e87381a578353
Mziwakx <ayoubmzi10@gmail.com>
2023-07-27 19:45:38 -0700

    all the tasks + advanced

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,22 +1,20 @@
-#include <stddef.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
  * list_len - Returns the number of elements in a linked list.
  * @h: Pointer to the list_t list.
  *
- * Return: Number of elements in the list.
+ * Return: Number of elements in h.
  */
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
-	const list_t *current = h;
+	size_t n = 0;
 
-	while (current != NULL)
+	while (h)
 	{
-	ount++;
-	urrent = current->next;
+		n++;
+		h = h->next;
 	}
-
-	return (count);
+	return (n);
 }
--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -10,26 +11,20 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	list_t *new;
 	unsigned int len = 0;
 
 	while (str[len])
-	len++;
+		len++;
 
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
-	return (NULL);
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-	new_node->str = strdup(str);
-	if (!new_node->str)
-	{
-	free(new_node);
-	return (NULL);
-	}
-
-	new_node->len = len;
-	new_node->next = *head;
-	*head = new_node;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
 	return (*head);
 }
